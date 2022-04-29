85d62427f688e5a0e2605a96e0aa187178394bfb
ohnadir <qx.nadir@gmail.com>
2022-04-29 17:29:13 +0600

    implement delete button

--- src/Component/AllInventories/AllInventories.js
+++ src/Component/AllInventories/AllInventories.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { BsTrash } from 'react-icons/bs';
+import usePerfumes from '../Hooks/usePerfumes';
 
 const AllInventories = ({perfume}) => {
-    const { name, img, price, stock } = perfume;
+    const { name, img, price, stock, _id } = perfume;
+    const [perfumes, setPerfumes] = usePerfumes();
+    const handleDeleteItem = (id) => {
+        const proceed = window.confirm('Are you Sure');
+        if (proceed) {
+            const url = `http://localhost:5000/perfume/${id}`
+            fetch(url, {
+                method: 'DELETE',
+            })
+            .then(res => res.json())
+                .then(data => {
+                    const remaining = perfumes.filter(item => item._id !== id);
+                    setPerfumes(remaining);
+            })
+        }
+    }
 
     return (
             <div className='w-[450px] mx-auto border mb-4'>
@@ -15,7 +31,7 @@ const AllInventories = ({perfume}) => {
                             <p>Stock:- {stock}</p>
                         </div>
                         <div className='absolute right-4 '>
-                            <button className='flex items-center justify-center h-[35px] bg-red-200 w-[35px] rounded-full text-red-600'><BsTrash className='text-xl'/></button>
+                            <button onClick={()=>handleDeleteItem(_id)} className='flex items-center justify-center h-[35px] bg-red-200 w-[35px] rounded-full text-red-600'><BsTrash className='text-xl'/></button>
                         </div>
                     </div>
                 </div>
